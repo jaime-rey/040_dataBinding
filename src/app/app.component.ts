1ec740ce79ac7ac532e9bd9875dc3d5bfc485f9a
jaime <jaimereycasado@gmail.com>
2023-05-22 01:58:14 +0200

    Data Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dataBinding';
+  Libro = {
+    "titulo":"Hamlet",
+    "autor":"William Shakespeare",
+    "precio":21.30,
+    "stock":5,
+    "cantidad":0,
+    "imagen":"assets/old-books.jpg"
+  };
+  downCantidad(libro: { cantidad: number; }){
+    if (libro.cantidad>0) {
+      libro.cantidad--;
+    }
+  }
+  upCantidad(libro: {cantidad: number; stock:number;}){
+    if (libro.cantidad<libro.stock) libro.cantidad++;
+  }
+  getCoord(event: { clientX: number; clientY: number; }) { console.log(event.clientX + ", " + event.clientY); }
 }
